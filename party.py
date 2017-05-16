ab691d849d63734c50020e6c1fb314a86da77f4f
Sergi Carol <sergi@nan-tic.com>
2017-05-16 15:36:23 +0200

    Fixed locales, added relate from party, added readonly fields, removed contrain but added check when approving a credit

--- party.py
+++ party.py
@@ -49,29 +49,45 @@ class PartyCredit(Workflow, ModelSQL, ModelView):
     'Party Credit'
     __name__ = 'party.credit'
 
-    party = fields.Many2One('party.party', 'Party', required=True)
+    party = fields.Many2One('party.party', 'Party', required=True,
+        states={
+            'readonly': Eval('state') == 'approved'
+        })
     # date when the party requested the credit
-    date = fields.Date('Requested Date', required=True)
+    date = fields.Date('Requested Date', required=True,
+        states={
+            'readonly': Eval('state') == 'approved'
+        })
     # date when the requested credit was approved by the
     # insurance company. start_date is introduced manually.
-    start_date = fields.Date('Start Date')
+    start_date = fields.Date('Start Date', states={
+            'readonly': Eval('state') == 'approved'
+            })
     # end_date: date when the approved credit expires.
     # end_date is introduced manually.
-    end_date = fields.Date('End Date')
+    end_date = fields.Date('End Date', states={
+            'readonly': Eval('state') == 'approved'
+            })
     # requested_credit_limit: amount of money requested by the party to the
     # insurance company
     requested_credit_limit = fields.Numeric('Requested Credit Limit',
-        digits=(16, 2), required=True)
+        digits=(16, 2), required=True, states={
+            'readonly': Eval('state') == 'approved'
+        })
     # approved_credit_limit: amount of money granted by the insurance company
     approved_credit_limit = fields.Numeric('Approved Credit Limit',
-        digits=(16, 2))
+        digits=(16, 2), states={
+            'readonly': Eval('state') == 'approved'
+        })
     # invoice_line: Link to Credit and Suretyship supplier invoice line
     # invoice_line = fields.Many2One('account.invoice.line')
     state = fields.Selection([
         ('requested', 'Requested'),
         ('approved', 'Approved'),
         ('rejected', 'Rejected'),
-    ], 'State', required=True)
+    ], 'State', required=True, states={
+            'readonly': Eval('state') == 'approved'
+            })
 
     # Returns the maximum risk amount registered for the given timeframe
     maximum_registered = fields.Function(fields.Numeric(
@@ -87,21 +103,16 @@ class PartyCredit(Workflow, ModelSQL, ModelView):
     # accounts_data = fields.Function(fields.One2Many('party.risk.analysis',
     #        'party_credit', 'Accounts'), 'on_change_with_accounts_data')
     accounts_data = fields.One2Many('party.risk.analysis',
-            'party_credit', 'Accounts')
+            'party_credit', 'Accounts', readonly=True)
 
     @classmethod
     def __setup__(cls):
         super(PartyCredit, cls).__setup__()
-        cls._sql_constrains = [
-            ('party_unique', 'Unique(party, state)', 'There can only be one '
-                'party with the same state')
-        ]
         # Error messages
         cls._error_messages.update({
-            'approved_credit_party': ('It is only allowed an approved credit '
+            'approved_party_credit': ('It is only allowed an approved credit '
                 'per party and the party credit "%(rec_name)s" you want to '
-                'add exceeds this limit. Approved party credits assigned to '
-                'the same party: "%(approved_party_credits)s"')
+                'add exceeds this limit.')
         })
         # Workflow transitions
         cls._transitions = set((
@@ -164,6 +175,15 @@ class PartyCredit(Workflow, ModelSQL, ModelView):
         Party = Pool().get('party.party')
         to_write = []
         for party_credit in party_credits:
+            duplicate = cls.search([
+                ('party', '=', party_credit.party.id),
+                ('start_date', '<=', party_credit.start_date),
+                ('end_date', '>=', party_credit.start_date),
+                ('state', '=', 'approved')], limit=1)
+            if duplicate:
+                cls.raise_user_error('approved_party_credit', {
+                        'rec_name': party_credit.rec_name
+                        })
             if (party_credit.party.company_credit_limit is not None and
             party_credit.approved_credit_limit is not None):
 
