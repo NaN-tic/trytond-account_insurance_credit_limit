2343696740cebb0cfb5f5f4d92adadbdbc4397c0
Sim? Albert i Beltran <sim6@probeta.net>
2018-05-14 21:54:41 +0200

    Improve tests

--- tests/test_account_insurance_credit_limit.py
+++ tests/test_account_insurance_credit_limit.py
@@ -3,10 +3,12 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
-class TestAccountInsuranceCreditLimitCase(unittest.TestCase):
+class TestAccountInsuranceCreditLimitCase(ModuleTestCase):
     'Test Account Insurance Credit Limit module'
     module = 'account_insurance_credit_limit'
 
@@ -18,5 +20,6 @@ def suite():
     suite.addTests(doctest.DocFileSuite(
             'scenario_account_insurance_credit_limit.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
