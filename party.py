34ff012d5fd9d158ee2b08a5b785dc169f273cf7
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2019-12-10 16:12:33 +0100

    Use direct access to backend classes.

--- party.py
+++ party.py
@@ -101,15 +101,14 @@ class PartyCompanyCreditLimit(ModelSQL, CompanyValueMixin):
     def __register__(cls, module_name):
         pool = Pool()
         Party = pool.get('party.party')
-        TableHandler = backend.get('TableHandler')
         cursor = Transaction().connection.cursor()
-        exist = TableHandler.table_exist(cls._table)
+        exist = backend.TableHandler.table_exist(cls._table)
         table = cls.__table__()
         party = Party.__table__()
         super(PartyCompanyCreditLimit, cls).__register__(module_name)
 
         if not exist:
-            party_h = TableHandler(Party, module_name)
+            party_h = backend.TableHandler(Party, module_name)
             if party_h.column_exist('company_credit_limit'):
                 query = table.insert(
                     [table.party, table.company_credit_limit],
