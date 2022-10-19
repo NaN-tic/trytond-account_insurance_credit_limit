44e4604b8f731afd1ecec41f15c9f137d1ee2dac
Jared Esparza <jared.esparza@nan-tic.com>
2022-10-19 11:21:05 +0200

    Modify end_date field to be required | #067482

--- party.py
+++ party.py
@@ -162,7 +162,8 @@ class PartyCredit(Workflow, ModelSQL, ModelView):
             })
     # end_date: date when the approved credit expires.
     # end_date is introduced manually.
-    end_date = fields.Date('End Date', states={
+    end_date = fields.Date('End Date', required=True,
+        states={
             'readonly': Eval('state') == 'approved'
             })
     # requested_credit_limit: amount of money requested by the party to the
